f304128ce5c81210c92184f2d3d4cd590bc30048
Sasan Dilantha <sasandilantha@gmail.com>
2023-08-24 15:29:49 +0530

    Create DB_Conector Class

---
+++ php/Classes/DB_Conector.php
@@ -0,0 +1,17 @@
+<?php
+
+class DB_Conector
+{
+
+    public static function  get_connection($HOST = "localhost",$DB_NAME = "mhs",$DB_USER = "root",$DB_PASSWORD = ""){
+        $dsn = "mysql:host=".$HOST.";dbname=".$DB_NAME;
+
+        try {
+            $option = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+            $connection = new PDO($dsn,$DB_USER,$DB_PASSWORD,$option);
+            return $connection;
+        }catch (PDOException $ex){
+            die($ex->getMessage());
+        }
+    }
+}
