0df3b2035e871a6b905fb4b893ad3c1b79b649d5
Sasan Dilantha <sasandilantha@gmail.com>
2023-08-24 13:34:55 +0530

    create stress level quaction ui

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ADMIN DASHBORD</title>
+    <?php include_once 'accest/links/MDB/css/mdb_css.php';?>
+
+    <link rel="stylesheet" href="accest/css/sub_1.css">
+
+
+
+</head>
+<body>
+<div class="container mt-5">
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary btn-rounded btn-lg" data-mdb-toggle="modal" data-mdb-target="#stress_1">
+        <i class="fas fa-brain"></i> test Stress
+    </button>
+
+    <?php include "pages/stress_level_1.php"?>
+</div>
+
+
+
+<?php include_once 'accest/links/MDB/js/mdb_js.php';?>
+<!-- Link to Chart.js library -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.5.1/chart.min.js"></script>
+<script src="accest/js/sub_1.js"></script>
+<script src="accest/js/main.js"></script>
+</body>
+</html>
---
+++ pages/stress_level_1.php
@@ -0,0 +1,44 @@
+<!-- Modal -->
+<div class="modal fade bg-glass_sub" id="stress_1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+
+        <div class="modal-content" style="width: auto">
+            <div class="modal-header">
+                <div class="d-flex justify-content-centered">
+                    <h5 class="modal-title fw-bold txt-gradient" id="exampleModalLabel">Test Your Mind Status</h5>
+                </div>
+                <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-4">
+                    <p class="fw-normal">In the last month, how often have you been angered because of things that happened that were outside of your control?</p>
+                </div>
+                <div class="md-btn-group-vertical d-flex flex-column flex-md-row bg-transparent btn-rounded">
+                    <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off">
+                    <label class="btn btn-outline-secondary btn-rounded m-2" for="option1">N</label>
+
+                    <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off">
+                    <label class="btn btn-outline-secondary btn-rounded  m-2"  for="option2">AN</label>
+
+                    <input type="radio" class="btn-check" name="options" id="option3" autocomplete="off">
+                    <label class="btn btn-outline-secondary btn-rounded m-2"  for="option3">S</label>
+
+                    <input type="radio" class="btn-check" name="options" id="option4" autocomplete="off">
+                    <label class="btn btn-outline-secondary btn-rounded m-2" for="option4">FO</label>
+
+                    <input type="radio" class="btn-check" name="options" id="option5" autocomplete="off">
+                    <label class="btn btn-outline-secondary btn-rounded m-2"  for="option5">VO</label>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <div class="note note-warning mb-3">
+                    <strong>Note :</strong> <small>N - Never |  AN - Almost Never | S - Sometimes | FO - Fairly Often | VO - Very Often</small>
+                </div>
+                <button type="button" class="btn btn-primary btn-floating"><i class="fas fa-angle-right"></i></button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
