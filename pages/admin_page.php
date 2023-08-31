895880a9aa6f8f8526921a9a1a70a8913bbe0940
dvp213 <108648545+dvp213@users.noreply.github.com>
2023-08-31 11:29:49 +0530

    Update admin_page.php

--- pages/admin_page.php
+++ pages/admin_page.php
@@ -7,7 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ADMIN DASHBORD</title>
     <?php include_once '../assets/links/MDB/css/mdb_css.php';?>
-
     <link rel="stylesheet" href="../assets/css/main.css">
     <link rel="stylesheet" href="../assets/css/sub_1.css">
 
