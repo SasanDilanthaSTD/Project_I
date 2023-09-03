023f2a467f7ba96bb1dd2c64bbe41b9e74e02c63
dvp213 <108648545+dvp213@users.noreply.github.com>
2023-09-03 13:11:14 +0530

    contact us

--- pages/notification_test.php
+++ pages/notification_test.php
@@ -8,38 +8,32 @@
     <title>Document</title>
     <script src="../assets/js/mail.js" type="text/javascript"></script>
 </head>
-<!--<script type="text/javascript"-->
-<!--        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js">-->
-<!--</script>-->
-<!--<script type="text/javascript">-->
-<!--    (function(){-->
-<!--        emailjs.init("fYPcLvIPnNJkxzpZu");-->
-<!--    })();-->
-<!--</script>-->
+<script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js">
+</script>
+<script type="text/javascript">
+    (function () {
+        emailjs.init("fYPcLvIPnNJkxzpZu");
+    })();
+</script>
 <body>
 <div class="container">
     <h1>Contact Us</h1>
-    <form action="../php/Classes/notification_mail.php" method="POST">
         <p>
             <label for="fullname">Full Name</label>
-            <input type="text" id="fullname" name="fullname" id="fullname" required>
+            <input type="text" id="fullname" name="fullname" required>
         </p>
         <p>
             <label for="email">Email</label>
-            <input type="email" id="email" name="email" id="email" required>
-        </p>
-        <p>
-            <label for="subject">Subject</label>
-            <input type="text" id="subject" name="subject" id="subject" required>
+            <input type="email" id="email_id" name="email_id" required>
         </p>
         <p>
             <label for="message">Message</label>
             <textarea name="message" id="message" cols="30" rows="10"></textarea>
         </p>
         <p>
-            <button type="submit" name="submit">Send</button>
+            <button onclick="SendMail()">Send</button>
         </p>
-    </form>
-</div><!-- container onclick="SendMail()"-->
+</div>
 </body>
 </html>
